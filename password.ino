e3a54eb7549906da1d88297de1f4ead6eab07c56
khj1030 <69004675+khj1030@users.noreply.github.com>
2020-11-26 13:53:38 +0900

    Update password.ino

--- password.ino
+++ password.ino
@@ -3,6 +3,19 @@
 #include<Servo.h>
 #include <SoftwareSerial.h>
 
+int Buzzer = 4;
+#define C 262 // 도 
+#define D 294 // 레 
+#define E 330 // 미 
+#define F 349 // 파 
+#define G 392 // 솔 
+#define A 440 // 라 
+#define B 494 // 시 
+
+int tempo = 200; // duration 옵션 값 설정 
+int notes[25] = { E,G,B }; 
+int notes2[25] = {B};
+
 SoftwareSerial bluetooth(8, 9); // 8번 TX, 9번 RX
 Servo servo;      
 int value = 0; 
@@ -22,10 +35,11 @@ byte up[8] = { B00010000, B00111000, B01010100, B00010000, B00010000, B00010000,
 byte right[8] ={B00000000,B00000100,B00000010,B11111111,B00000010,B00000100,B00000000,B00000000};//오른쪽
 byte down[8] = { B00010000, B00010000, B00010000, B00010000, B00010000, B01010100, B00111000, B00010000};//아래
 byte left[8] ={B00000000,B00100000,B01000000,B11111111,B01000000,B00100000,B00000000,B00000000};//왼쪽
-byte o[8] = {B01110000, B10001000, B10001000, B10001000, B10001000, B10001000, B01110000};
-byte x[8] = {B10001000, B10001000, B01010000, B00100000, B01010000,B10001000, B10001000};
+byte o[8] = {B00000000, B00111100, B01000010,  B01000010,  B01000010,  B01000010,  B00111100};
+byte x[8] = {B00000000, B01000010, B00100100, B00011000, B00011000,B00100100, B01000010};
 
 void setup(){
+   pinMode(Buzzer,OUTPUT);
   bluetooth.begin(9600);
   servo.attach(7);
   lc.shutdown(0, false);
@@ -44,7 +58,7 @@ void loop(){
 
   if(val == 'a')
   {
-     servo.write(0);
+     servo.write(20);
   }
   if(val == 'b')
   {
@@ -53,35 +67,35 @@ void loop(){
 
 
   if (buttonpressed == 0){
-     servo.write(0);
+     servo.write(20);
   }
-  if(yval >= 800){ //위
+  if(yval >= 800){ //위 //왼
     for (int row = 0; row < 8; row++){
       lc.setRow(0, row, up[row]);
       delay(25);
     }
-    key = '0';
+    key = '3'; //3
   }
-  else if (yval <=250){ //아래
+  else if (yval <=250){ //아래 //오
     for (int row = 0; row < 8; row++){
       lc.setRow(0, row, down[row]);
       delay(25);
     }
-    key = '2';   
+    key = '1';   //1
   }
-  else if (xval >= 800){ //왼쪽
+  else if (xval >= 800){ //왼쪽 //아래
     for (int row = 0; row < 8; row++){
       lc.setRow(0, row, left[row]);
       delay(25);
     }
-    key = '3';   
+    key = '2';   //2
   }
-  else if (xval <= 250) { //오른쪽
+  else if (xval <= 250) { //오른쪽 //위
     for (int row = 0; row < 8; row++){
       lc.setRow(0, row, right[row]);
       delay(25);
     }
-    key = '1';  
+    key = '0';  //0
   }
   else {
     key = '~'; 
@@ -97,6 +111,7 @@ void loop(){
     else if(key != secretCode[position]){ // 해당 자리에 맞지 않는 비밀번호가 입력됬을 경우
       position++; /**********/
       wrong++; // 비밀번호 오류 값을 늘려준다
+      
     }
  
     if(position == 4){ // 4자리 비밀번호가 모두 맞았을 경우
@@ -105,6 +120,8 @@ void loop(){
           lc.setRow(0, row, x[row]);
           delay(25);
         }
+        tone (Buzzer, notes[ 0 ], tempo); 
+        delay(300); 
       } 
       else {
         for (int row = 0; row < 8; row++){
@@ -112,7 +129,11 @@ void loop(){
           delay(25);
         }
         servo.write(120);
-      }
+        for (int i = 0; i < 3; i++) { 
+          tone (Buzzer, notes[ i ], tempo); 
+          delay(300); 
+        }
+      } 
       position = 0;
       wrong =0;
       delay(1000);
